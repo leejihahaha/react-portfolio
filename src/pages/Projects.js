da14b512b36124662b34caa1adc5175f5bbe3496
jiha <zhsclq5324@naver.com>
2024-02-15 14:21:41 +0900

    네번째 커밋

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,27 +1,20 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import styles from "../styles/Projects.module.css";
 import ProjectCard from "../components/ProjectCard";
+import { useQuery } from "@tanstack/react-query";
+import { fetchData } from "../api";
 
 export default function Projects() {
-  const [project, setProject] = useState(null);
-
-  async function fetchData() {
-    //캡슐화
-    const response = await (
-      await fetch(
-        "https://raw.githubusercontent.com/leejihahaha/myapi/master/myapi/project.json"
-      )
-    ).json();
-    setProject(response);
-    console.log(response);
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const {
+    isLoading,
+    data: project,
+    error,
+  } = useQuery({ queryKey: "project", queryFn: fetchData });
 
   return (
     <>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
       <div className={styles.container}>
         {project &&
           project.map((item, idx) => {
